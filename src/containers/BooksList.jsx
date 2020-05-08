43269d47107e6a0059f99c5f73b14ba9e122fcb4
J. Christian Romero <jcromerohdz@gmail.com>
2020-05-08 13:58:16 -0700

    Fixing eslint error

--- src/containers/BooksList.jsx
+++ src/containers/BooksList.jsx
@@ -3,24 +3,22 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
 
-const BooksList = ({ books }) => {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Book ID</th>
-            <th>Title</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {books.length > 0 && books.map(book => <Book key={book.id} book={book} />)}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+const BooksList = ({ books }) => (
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.length > 0 && books.map(book => <Book key={book.id} book={book} />)}
+      </tbody>
+    </table>
+  </div>
+);
 
 BooksList.propTypes = { books: PropTypes.instanceOf(Array).isRequired };
 
