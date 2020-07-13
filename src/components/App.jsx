20938f6c07e0a62da41e2db8c27ef310c66f8493
J. Christian Romero <jcromerohdz@gmail.com>
2020-07-13 14:52:29 -0700

    fixin eslint errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import BookList from '../containers/BooksList';
-import BookForm from '../containers/BooksForm';
+import React from 'react'
+import BookList from '../containers/BooksList'
+import BookForm from '../containers/BooksForm'
 
 function App() {
   return (
