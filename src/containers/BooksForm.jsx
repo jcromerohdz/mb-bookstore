4a77e268981e6c71f0cf786f454e96f6e8669b9a
J. Christian Romero <jcromerohdz@gmail.com>
2020-07-13 14:39:14 -0700

    Just testing to see if stickler works

--- src/containers/BooksForm.jsx
+++ src/containers/BooksForm.jsx
@@ -15,3 +15,28 @@ const BookForm = () => (
 );
 
 export default BookForm;
+import PropTypes from 'prop-types';
+import Book from '../components/Book';
+
+const BooksList = ({ books }) => (
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.length > 0 && books.map(book => <Book key={book.id} book={book} />)}
+      </tbody>
+    </table>
+  </div>
+);
+
+BooksList.propTypes = { books: PropTypes.instanceOf(Array).isRequired };
+
+const mapStateToProps = state => ({ books: state.books });
+
+export default connect(mapStateToProps)(BooksList);
