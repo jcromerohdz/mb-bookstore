841a592fe1b6bb8e4f41f98ef22cc11d879a53ee
J. Christian Romero <jcromerohdz@gmail.com>
2020-07-22 11:33:53 -0700

    Adding All to categories list

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,2 +1,2 @@
-const categories = ['Action', 'Adventure', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const categories = ['All', 'Action', 'Adventure', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 export default categories;
